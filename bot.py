129c3d864704cdfe9fa79fc201504674889b439d
Nuginity <nuginity15@gmail.com>
2025-01-13 14:46:00 +0700

    add bot arguments, and update to 1.0.1

--- bot.py
+++ bot.py
@@ -3,8 +3,18 @@ from discord.ext import commands
 import src.events
 import src.util
 import src.com
-from src.util import config
+import argparse
 
+# Parse arguments
+parser = argparse.ArgumentParser(description="Run the Discord bot with a specific token.")
+parser.add_argument("token", type=str, nargs="?", help="The Discord bot token.")
+args = parser.parse_args()
+
+# Load configuration
+config = src.util.config
+
+# Determine token to use
+token = args.token if args.token else config['DISCORD_TOKEN']
 
 # Konfigurasi intents
 intents = discord.Intents.all()
@@ -16,5 +26,5 @@ bot = commands.Bot(command_prefix=config['PREFIX'], intents=intents)
 src.events.setup(bot)
 src.com.setup(bot)
 
-bot.run(config['DISCORD_TOKEN'])
-
+# Run bot with the determined token
+bot.run(token)
